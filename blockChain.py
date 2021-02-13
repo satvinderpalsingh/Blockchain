312e56948f12612b234f64faf169ccd9530ff28d
Satvinder Pal Singh <satvinderpalsingh52663@gmail.com>
2021-02-13 10:31:20 +0530

    comments

--- blockChain.py
+++ blockChain.py
@@ -38,20 +38,21 @@ class Blockchain:
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
-    
+
     def is_chain_valid(self, chain):
-        previous_block = chain[0]
-        block_index = 1
-        while block_index < len(chain):
-            block = chain[block_index]
-            if block['previous_hash'] != self.hash(previous_block):
+        previous_block = chain[0] # reference of first block stored genesis block
+        block_index = 1 # required for iteration
+        while block_index < len(chain): 
+            block = chain[block_index] # cuurent block
+            if block['previous_hash'] != self.hash(previous_block): # checking weather the refernce stored in property previus_hash is currently matched or not with the hash of previous block using hash function
                 return False
             previous_proof = previous_block['proof']
             proof = block['proof']
-            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            # verfying the proof of block with the data proof and previous proof
+            hash_operation = hashlib.sha256(
+                str(proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] != '0000':
                 return False
             previous_block = block
             block_index += 1
         return True
-        
