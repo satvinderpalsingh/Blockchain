5137c3e0387eb487c47abceb92b0ae99e7185b7b
Satvinder Pal Singh <satvinderpalsingh52663@gmail.com>
2021-02-13 13:03:31 +0530

    mining_block

--- blockChain.py
+++ blockChain.py
@@ -4,6 +4,8 @@ import json  # to communicate in json data
 # Flask to implement webservices jsonify to see the jsop message/response
 from flask import Flask, jsonify
 
+# Building a Blockchain
+
 
 class Blockchain:
 
@@ -21,7 +23,7 @@ class Blockchain:
         self.chain.append(block)
         return block
 
-    def get_previous_hash(self):
+    def get_previous_block(self):
         return self.chain[-1]
 
     def proof_of_work(self, previous_proof):
@@ -34,17 +36,20 @@ class Blockchain:
                 check_proof = True
             else:
                 new_proof += 1
+        return new_proof
 
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
 
     def is_chain_valid(self, chain):
-        previous_block = chain[0] # reference of first block stored genesis block
-        block_index = 1 # required for iteration
-        while block_index < len(chain): 
-            block = chain[block_index] # cuurent block
-            if block['previous_hash'] != self.hash(previous_block): # checking weather the refernce stored in property previus_hash is currently matched or not with the hash of previous block using hash function
+        # reference of first block stored genesis block
+        previous_block = chain[0]
+        block_index = 1  # required for iteration
+        while block_index < len(chain):
+            block = chain[block_index]  # cuurent block
+            # checking weather the refernce stored in property previus_hash is currently matched or not with the hash of previous block using hash function
+            if block['previous_hash'] != self.hash(previous_block):
                 return False
             previous_proof = previous_block['proof']
             proof = block['proof']
@@ -56,3 +61,50 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
+
+# Mining our Blockchain
+
+app = Flask(__name__)
+
+# Creating a Blockchain
+# creating the instance of blockchain
+blockchain = Blockchain()
+
+# Mining the blockchain
+
+@app.route('/mine_block',methods=['GET'])
+def mine_block():
+    previous_block=blockchain.get_previous_block()
+    previous_proof=previous_block['proof']
+    proof=blockchain.proof_of_work(previous_proof)
+    previous_hash=blockchain.hash(previous_block)
+    block=blockchain.create_block(proof,previous_hash)
+    response = {'message': 'Congratulations, you just mined a block! 😈😈😈😈😈🤓🤓🤓',## response is a json data
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+#getting all blocks in chain
+
+@app.route('/get_chain',methods=['GET'])
+def get_chain():
+    response={
+        'chain':blockchain.chain,
+        'length':len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+# custom message
+
+@app.route('/',methods=['GET'])
+def custom_message():
+    response={
+        'message':'Congratulations you are on Whalecoin 🐳🐳🐳🐳🐳🐳'
+    }
+    return jsonify(response), 200
+
+# Running the app
+
+app.run(host = '0.0.0.0', port = 5000) #host= '0.0.0.0' specifies that it is available publicily
